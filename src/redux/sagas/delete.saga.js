b8bd1d37af4e4e8eb133627c9a410d3862f97772
Davey-M <davey.meuer@gmail.com>
2022-03-10 11:37:16 -0600

    fix breaking delete change

--- src/redux/sagas/delete.saga.js
+++ src/redux/sagas/delete.saga.js
@@ -4,17 +4,17 @@ const { takeLatest, put } = require("redux-saga/effects");
 
 
 function* deleteItem(action) {
-    try {
-        //Sending the item to the server to delete
-        yield axios.delete('/api/shelf', action.payload )
-        yield put({type: 'FETCH_ITEMS'})
-    } catch(error) {
-        console.log('Error in Delete Item Saga', error); 
-    }
+  try {
+    //Sending the item to the server to delete
+    yield axios.delete('/api/shelf', action.payload)
+    yield put({ type: 'FETCH_ITEMS' })
+  } catch (error) {
+    console.log('Error in Delete Item Saga', error);
+  }
 }
 
 function* deleteItemSaga() {
-    yield takeLatest({type: 'DELETE_ITEM', deleteItem})
+  yield takeLatest('DELETE_ITEM', deleteItem);
 }
 
 
