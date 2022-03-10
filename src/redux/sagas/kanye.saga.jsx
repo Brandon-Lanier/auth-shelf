c7c9a211b3978ebb5bc9062e77ea8133f34a9e73
Brandon Lanier <brandonjlanier@gmail.com>
2022-03-10 14:56:08 -0600

    Kanye

---
+++ src/redux/sagas/kanye.saga.jsx
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const { takeLatest, put } = require("redux-saga/effects");
+
+
+
+function* getKanyeSaga() {
+    takeLatest('GET_KANYE', getKanye);
+}
+
+function* getKanye() {
+    try {
+        const kanyeQuote = yield axios.get('https://api.kanye.rest');
+        yield put({type: 'SET_KANYE', payload: kanyeQuote.quote})
+    } catch(error) {
+        console.log('Error getting Kanye West', error);
+    }
+}
+
+
+export default getKanyeSaga;
+
