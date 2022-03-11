eb67d49c552c8dedff82291ed08734e91268b9cb
Davey-M <davey.meuer@gmail.com>
2022-03-11 11:07:18 -0600

    add mui to add item form

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -1,42 +1,63 @@
-import { useDispatch, } from "react-redux"
-import { useState } from "react"
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+import { Button, TextField } from '@mui/material';
+
+import './addItemForm.css';
 
 function AddItem() {
-    
-    const [description, setDescription] = useState('')
-    const [imageUrl, setImageUrl] = useState('')
+  const [description, setDescription] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    console.log(description, imageUrl);
+  console.log(description, imageUrl);
 
-    const handleSubmit = () => {
-        console.log('clicked');
+  const handleSubmit = () => {
+    console.log('clicked');
 
-        dispatch({
-            type: 'POST_ITEM',
-            payload: {description: description, image_url: imageUrl}
-        })
+    dispatch({
+      type: 'POST_ITEM',
+      payload: { description: description, image_url: imageUrl },
+    });
 
-        setDescription('')
-        setImageUrl('')
-    }
+    setDescription('');
+    setImageUrl('');
+  };
 
-    return (
-        <>
-            <input 
-            type="text"
-            value={description}
-            placeholder="Description"
-            onChange={(event) => setDescription(event.target.value)}/>
-            <input 
-            type="text"
-            placeholder="Image Url(OPTIONAL)"
-            value={imageUrl}
-            onChange={(event) => setImageUrl(event.target.value)}/>
-            <button onClick={handleSubmit}>Add To Shelf</button>
-        </>
-    )
+  return (
+    <div className='form-container'>
+      <TextField
+        type='text'
+        value={description}
+        placeholder='Description'
+        variant='outlined'
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <TextField
+        type='text'
+        placeholder='Image Url(OPTIONAL)'
+        value={imageUrl}
+        onChange={(event) => setImageUrl(event.target.value)}
+      />
+      {/* <input
+        type='text'
+        value={description}
+        placeholder='Description'
+        onChange={(event) => setDescription(event.target.value)}
+      /> */}
+      {/* <input
+        type='text'
+        placeholder='Image Url(OPTIONAL)'
+        value={imageUrl}
+        onChange={(event) => setImageUrl(event.target.value)}
+      /> */}
+      {/* <button onClick={handleSubmit}>Add To Shelf</button> */}
+      <Button variant='contained' onClick={handleSubmit}>
+        Add to Shelf
+      </Button>
+    </div>
+  );
 }
 
-export default AddItem
+export default AddItem;
