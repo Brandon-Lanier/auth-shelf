76688d8352482baa8a475dd0b8b838a0e3a3c02b
Davey-M <davey.meuer@gmail.com>
2022-03-11 11:20:04 -0600

    add card preview to add item view

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -1,7 +1,14 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 
-import { Button, TextField } from '@mui/material';
+import {
+  Button,
+  TextField,
+  Card,
+  CardMedia,
+  CardHeader,
+  CardContent,
+} from '@mui/material';
 
 import './addItemForm.css';
 
@@ -26,37 +33,54 @@ function AddItem() {
   };
 
   return (
-    <div className='form-container'>
-      <TextField
-        type='text'
-        value={description}
-        placeholder='Description'
-        variant='outlined'
-        onChange={(event) => setDescription(event.target.value)}
-      />
-      <TextField
-        type='text'
-        placeholder='Image Url(OPTIONAL)'
-        value={imageUrl}
-        onChange={(event) => setImageUrl(event.target.value)}
-      />
-      {/* <input
+    <>
+      <div className='form-container'>
+        <TextField
+          type='text'
+          value={description}
+          placeholder='Description'
+          variant='outlined'
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <TextField
+          type='text'
+          placeholder='Image Url(OPTIONAL)'
+          value={imageUrl}
+          onChange={(event) => setImageUrl(event.target.value)}
+        />
+        {/* <input
         type='text'
         value={description}
         placeholder='Description'
         onChange={(event) => setDescription(event.target.value)}
       /> */}
-      {/* <input
+        {/* <input
         type='text'
         placeholder='Image Url(OPTIONAL)'
         value={imageUrl}
         onChange={(event) => setImageUrl(event.target.value)}
       /> */}
-      {/* <button onClick={handleSubmit}>Add To Shelf</button> */}
-      <Button variant='contained' onClick={handleSubmit}>
-        Add to Shelf
-      </Button>
-    </div>
+        {/* <button onClick={handleSubmit}>Add To Shelf</button> */}
+        <Button variant='contained' onClick={handleSubmit}>
+          Add to Shelf
+        </Button>
+      </div>
+      <div className='card-container'>
+        <Card style={{ minWidth: '200px' }}>
+          {/* <CardHeader> */}
+          <CardMedia
+            component='img'
+            height='194'
+            image={imageUrl}
+            alt={imageUrl}
+          />
+          {/* </CardHeader> */}
+          <CardContent>
+            <p>{description}</p>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
 
