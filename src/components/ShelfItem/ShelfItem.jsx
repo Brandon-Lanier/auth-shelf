3d8591d12690da59eca75e71400ae96ab76b5581
Davey-M <davey.meuer@gmail.com>
2022-03-11 09:31:41 -0600

    format spaces

--- src/components/ShelfItem/ShelfItem.jsx
+++ src/components/ShelfItem/ShelfItem.jsx
@@ -13,68 +13,68 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useSelector } from 'react-redux';
 
 function ShelfItem({ item }) {
-    const ExpandMore = styled((props) => {
-        const { expand, ...other } = props;
-        return <IconButton {...other} />;
-    })(({ theme, expand }) => ({
-        transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    }));
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
 
-    const user = useSelector((store) => store.user);
+  const user = useSelector((store) => store.user);
 
-    const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component='img'
-                height='194'
-                image={item.image_url}
-                alt={'Whoops it looks like there was an error'}
-            />
-            <CardContent>
-                <Typography variant='body2' color='text.secondary'>
-                    {item.description}
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                {/* <IconButton aria-label="Edit">
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component='img'
+        height='194'
+        image={item.image_url}
+        alt={'Whoops it looks like there was an error'}
+      />
+      <CardContent>
+        <Typography variant='body2' color='text.secondary'>
+          {item.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* <IconButton aria-label="Edit">
           <EditIcon />
         </IconButton> */}
-                {/* <IconButton aria-label="Complete Task">
+        {/* <IconButton aria-label="Complete Task">
           <DoneIcon />
         </IconButton> */}
-                <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label='show more'
-                >
-                    <ExpandMoreIcon />
-                </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout='auto' unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Consider the following:</Typography>
-                    <Typography paragraph>
-                        <i>{item.kanyeQuote}</i>
-                    </Typography>
-                    {item.username ? (
-                        <Typography align='right'>-{item.username}</Typography>
-                    ) : (
-                        <Typography align='right'>-Yeezy</Typography>
-                    )}
-                </CardContent>
-            </Collapse>
-        </Card>
-    );
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label='show more'
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Consider the following:</Typography>
+          <Typography paragraph>
+            <i>{item.kanyeQuote}</i>
+          </Typography>
+          {item.username ? (
+            <Typography align='right'>- {item.username}</Typography>
+          ) : (
+            <Typography align='right'>- Yeezy</Typography>
+          )}
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
 export default ShelfItem;
